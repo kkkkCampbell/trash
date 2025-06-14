d2b03901d2764cc725293b69b691f71250d4b6eb
kkkkCampbell <memberof@gmail.com>
2025-06-14 13:52:39 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 PS1_BAK="$PS1" && PS1="" && clear
+echo INSTALLED
+echo ==========
 opkg list-installed | grep -i -E "podkop|unblock|zapret|ruantiblock"
+
 service youtubeUnblock stop
 service zapret stop
 service ruantiblock stop
 [ -n "$(opkg find podkop | grep '0.2.5')" ] && { service https-dns-proxy start; service https-dns-proxy enable; } || { service https-dns-proxy stop; service https-dns-proxy disable; }
 service podkop restart && sleep 10
+echo NETWORK_TEST
+echo =============
 ping -q -c 1 ya.ru
 ping -q -c 1 8.8.8.8
 ping -I awg10 -q -c 1 ya.ru
