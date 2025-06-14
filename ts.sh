4ee6f872313ed3e66bbc449d60f896c3cd4c8c74
kkkkCampbell <memberof@gmail.com>
2025-06-14 18:27:50 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -16,7 +16,7 @@ printf "${green}youtubeUnblock: ${reset}" && service youtubeUnblock stop
 printf "${green}zapret: ${reset}" && service zapret stop
 printf "${green}ruantiblock: ${reset}" && service ruantiblock stop
 printf "${green}DoH: ${reset}" && [ -n "$(opkg find podkop | grep '0.2.5')" ] && { service https-dns-proxy start; service https-dns-proxy enable; } || { service https-dns-proxy stop; service https-dns-proxy disable; }
-printf "${green}podkop [restart]: ${reset}" && service podkop restart >/dev/null 2>&1 && sleep 10 && service podkop status && echo
+printf "${green}podkop [restart]: ${reset}" && service podkop restart >/dev/null 2>&1 && sleep 10 && service podkop status
 echo NETWORK_TEST
 echo ============
 printf "${green}PING DIRECT: ${reset}" && ping -q -c 1 $test_site 
