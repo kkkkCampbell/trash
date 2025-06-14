c1ab4aa4baea5045cadfb3d3939aa7e588b89176
kkkkCampbell <memberof@gmail.com>
2025-06-14 22:09:17 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -39,7 +39,7 @@ printf "${green}OPERA-PROXY [ $test_site ]: ${reset}" && curl -s -x http://127.0
 echo
 
 printf "${green}OPERA-PROXY-COUNTRY [ $ip_check_site ]: ${reset}" && curl -s -x http://127.0.0.1:18080 $ip_check_site | grep -i -E "country|message"
-printf "${green}AWG-IFACE-COUNTRY [ $ip_check_site ]:   ${reset}" && curl --interface awg10 -s ipinfo.io | grep  -i -E "country|message"
+#printf "${green}AWG-IFACE-COUNTRY [ $ip_check_site ]:   ${reset}" && curl --interface awg10 -s ipinfo.io | grep  -i -E "country|message"
 echo
 
 PS1="$PS1_BAK"
