be15af51c6547e1cabcf8532c248f10cbd38d216
kkkkCampbell <memberof@gmail.com>
2025-06-14 17:54:47 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -1,4 +1,4 @@
-ch#!/bin/sh
+p ch#!/bin/sh
 
 green='\033[32m'
 reset='\033[0m'
@@ -17,7 +17,7 @@ printf "${green}youtubeUnblock: ${reset}" && service youtubeUnblock stop
 printf "${green}zapret: ${reset}" && service zapret stop
 printf "${green}ruantiblock: ${reset}" && service ruantiblock stop
 printf "${green}DoH: ${reset}" && [ -n "$(opkg find podkop | grep '0.2.5')" ] && { service https-dns-proxy start; service https-dns-proxy enable; } || { service https-dns-proxy stop; service https-dns-proxy disable; }
-printf "${green}podkop [restart]: ${reset}" && service podkop restart >/dev/null 2>&1 && sleep 10 && echo
+printf "${green}podkop [restart]: ${reset}" && service podkop restart | grep service_triggers start && sleep 10 && echo
 echo
 
 echo NETWORK_TEST
