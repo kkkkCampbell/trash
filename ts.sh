f717ae95d9b64acaac2b203e359df742f314a20f
kkkkCampbell <memberof@gmail.com>
2025-06-14 15:26:46 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -2,6 +2,7 @@
 
 green='\033[32m'
 reset='\033[0m'
+test_site="https://ya.ru "
 
 PS1_BAK="$PS1" && PS1="" && clear
 echo INSTALLED
@@ -24,8 +25,8 @@ ping -q -c 1 ya.ru
 ping -q -c 1 8.8.8.8
 ping -I awg10 -q -c 1 ya.ru
 ping -I awg10 -q -c 1 8.8.8.8
-printf "${green}DIRECT: ${reset}" && wget -T 5 -qO- https://ya.ru | head -c 50 && echo
-printf "${green}SING-BOX: ${reset}" && http_proxy="http://127.0.0.1:18080" wget -T 5 -qO- https://ya.ru | head -c 50 && echo
+printf "${green}DIRECT [$test_site]: ${reset}" && wget -T 5 -qO- $test_site | head -c 50 && echo
+printf "${green}OPERA-PROXY [$test_site]: ${reset}" && http_proxy="http://127.0.0.1:18080" wget -T 5 -qO- $test_site | head -c 50 && echo
 PS1="$PS1_BAK"
 echo DONE
 echo
