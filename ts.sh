d890613e813cd2771c1371596106d0895be4594d
kkkkCampbell <memberof@gmail.com>
2025-06-14 19:01:16 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -9,6 +9,7 @@ PS1_BAK="$PS1" && PS1="" && clear
 echo INSTALLED
 echo =========
 opkg list-installed | grep -i -E "podkop|unblock|zapret|ruantiblock"
+echo
 
 echo STOPPING UNNECESSARY SERVICES
 echo =============================
@@ -17,6 +18,7 @@ printf "${green}zapret: ${reset}" && service zapret stop
 printf "${green}ruantiblock: ${reset}" && service ruantiblock stop
 printf "${green}DoH: ${reset}" && [ -n "$(opkg find podkop | grep '0.2.5')" ] && { service https-dns-proxy start; service https-dns-proxy enable; } || { service https-dns-proxy stop; service https-dns-proxy disable; }
 printf "${green}podkop [restart]: ${reset}" && service podkop restart >/dev/null 2>&1 && sleep 10 && service podkop status
+echo
 
 echo NETWORK_TEST
 echo ============
@@ -25,12 +27,16 @@ printf "${green}PING DIRECT: ${reset}" && ping -q -c 1 8.8.8.8
 printf "${green}PING AWG10: ${reset}" && ping -I awg10 -q -c 1 $test_site
 printf "${green}PING AWG10: ${reset}" && ping -I awg10 -q -c 1 8.8.8.8 
 echo
+
 printf "${green}DIRECT [ $test_site ]: ${reset}" && curl -s $test_site | head -c 50
 echo
+
 printf "${green}AWG10 [ $test_site ]: ${reset}" && curl --interface awg10 -s $test_site | head -c 50
 echo
+
 printf "${green}OPERA-PROXY [ $test_site ]: ${reset}" && curl -s -x http://127.0.0.1:18080 $test_site | head -c 50
 echo
+
 printf "${green}OPERA-PROXY-COUNTRY [ $ip_check_site ]: ${reset}" && curl -s -x http://127.0.0.1:18080 https://ipinfo.io/ | grep country
 PS1="$PS1_BAK"
 echo DONE
