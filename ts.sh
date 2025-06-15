74ff0c44822811a7225e2004af18f3fdaabf9734
kkkkCampbell <memberof@gmail.com>
2025-06-15 19:25:11 +0300

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -42,32 +42,38 @@ test_ip="8.8.8.8"
 ip_check_site="ipinfo.io"
 clear
 
-# Ищем интерфейсы, где ПРОТОКОЛ (proto) содержит 'amnezia' или 'vpn'
-interfaces=$(ubus call network.interface dump | jsonfilter -e '@.interface[*]' | while read -r iface; do
-    proto=$(echo "$iface" | jsonfilter -e '@.proto')
-    echo "$proto" | grep -qi -E 'amnezia|vpn' && \
-    echo "$iface" | jsonfilter -e '@.interface'
-done)
 
-# Проверяем и выводим список
-if [ -z "$interfaces" ]; then
-    echo "Интерфейсы с VPN-протоколами не найдены"
-    exit 1
+# проверяем наличие интерфейса awg10, при его отсутствии прекращаем выполнение скрипта
+if ! ip a show dev awg10 >/dev/null 2>&1; then
+
+    # Ищем интерфейсы, где ПРОТОКОЛ (proto) содержит 'amnezia' или 'vpn'
+    interfaces=$(ubus call network.interface dump | jsonfilter -e '@.interface[*]' | while read -r iface; do
+        proto=$(echo "$iface" | jsonfilter -e '@.proto')
+        echo "$proto" | grep -qi -E 'amnezia|vpn' && \
+        echo "$iface" | jsonfilter -e '@.interface'
+    done)
+    
+    # Проверяем и выводим список
+    if [ -z "$interfaces" ]; then
+        echo "Интерфейсы с VPN-протоколами не найдены"
+        exit 1
+    fi
+    
+    echo "Выберите номер нужного вам VPN-интерфейса, затем нажмите Enter:"
+    echo ================================================================
+    count=1
+    echo "$interfaces" | while read -r ifname; do
+        echo "$count. $ifname"
+        count=$((count+1))
+    done
+    
+    # Выбор и запись в переменную
+    read -p "Номер интерфейса: " num
+    vpn_iface=$(echo "$interfaces" | sed -n "${num}p")
+    
+    [ -n "$vpn_iface" ] && echo "Выбрано: $vpn_iface" || { echo "Ошибка"; exit 1; }
+
 fi
-
-echo "Выберите номер нужного вам VPN-интерфейса, затем нажмите Enter:"
-echo ================================================================
-count=1
-echo "$interfaces" | while read -r ifname; do
-    echo "$count. $ifname"
-    count=$((count+1))
-done
-
-# Выбор и запись в переменную
-read -p "Номер интерфейса: " num
-vpn_iface=$(echo "$interfaces" | sed -n "${num}p")
-
-[ -n "$vpn_iface" ] && echo "Выбрано: $vpn_iface" || { echo "Ошибка"; exit 1; }
 clear
 
 
@@ -76,9 +82,6 @@ echo INSTALLED
 echo ==========
 opkg list-installed | grep -i -E "podkop|unblock|zapret|ruantiblock|clash|passwall"
 echo
-
-# проверяем наличие интерфейса awg10, при его отсутствии прекращаем выполнение скрипта
-#ip a show dev awg10 >/dev/null 2>&1 || { echo -e "${yellow}Error: Интерфейс awg10 не обнаружен.${reset} Роутер настроен не скриптом №4." >&2; #exit 1; }
 
 
 echo STOPPING UNNECESSARY SERVICES
@@ -99,15 +102,15 @@ printf "${green}PING ${vpn_iface}  [ $test_site ]: ${reset}" && ping -I $vpn_ifa
 printf "${green}PING ${vpn_iface}  [ 8.8.8.8 ]:    ${reset}" && ping -I $vpn_iface -q -c 2 $test_ip | grep loss
 echo
 
-printf "${green}DIRECT [ $test_site ]: ${reset}     " && curl -s $test_site | head -c 12
+printf "${green}DIRECT [ $test_site ]: ${reset}     " && curl -s --max-time 10 $test_site | head -c 12
 echo
-printf "${green}AWG10  [ $test_site ]: ${reset}     " && curl --interface ${vpn_iface} -s $test_site | head -c 12
+printf "${green}AWG10  [ $test_site ]: ${reset}     " && curl -s --max-time 10 --interface ${vpn_iface} $test_site | head -c 12
 echo
-printf "${green}VLESS  [ $test_site ]:      ${reset}" && curl -s 127.0.0.1:1602 $test_site | head -c 12
+printf "${green}VLESS  [ $test_site ]:      ${reset}" && curl -s --max-time 10 127.0.0.1:1602 $test_site | head -c 12
 echo
-printf "${green}OPERA-PROXY [ $test_site ]: ${reset}" && curl -s -x http://127.0.0.1:18080 $test_site | head -c 12
+printf "${green}OPERA-PROXY [ $test_site ]: ${reset}" && curl -s --max-time 10 -x http://127.0.0.1:18080 $test_site | head -c 12
 echo
-printf "${green}OPERA-PROXY-COUNTRY [ $ip_check_site ]: ${reset}" && curl -s -x http://127.0.0.1:18080 $ip_check_site | grep -i -E "country|message"
+printf "${green}OPERA-PROXY-COUNTRY [ $ip_check_site ]: ${reset}" && curl -s --max-time 10 -x http://127.0.0.1:18080 $ip_check_site | grep -i -E "country|message"
 #printf "${green}AWG-IFACE-COUNTRY [ $ip_check_site ]:   ${reset}" && curl --interface ${vpn_iface} -s ipinfo.io | grep  -i -E "country|message"
 echo DONE
 echo
