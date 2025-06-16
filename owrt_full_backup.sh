852d1e83abc318450e926995773124da31ab1ee3
kkkkCampbell <memberof@gmail.com>
2025-06-16 22:17:36 +0300

    Create owrt_full_backup.sh

---
+++ owrt_full_backup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+ARCHIVE_USER="archive"
+PASSWORD="$ARCHIVE_USER"
+ARCHIVE_DIR="/tmp/archive"
+ARCHIVE_FILE="$ARCHIVE_DIR/overlay.tar"
+SHARE_NAME="archive"
+
+# Цвета (для ash через printf)
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# Проверка и запуск службы ksmbd
+/etc/init.d/ksmbd status >/dev/null 2>&1 || {
+    echo "▶️ Запускаю ksmbd..."
+    /etc/init.d/ksmbd start
+    /etc/init.d/ksmbd enable
+}
+
+echo "📁 Создаю директорию $ARCHIVE_DIR..."
+mkdir -p "$ARCHIVE_DIR"
+chmod 0777 "$ARCHIVE_DIR"
+
+echo "🗃️ Создание архива /overlay в $ARCHIVE_FILE..."
+tar -cvhpf "$ARCHIVE_FILE" /overlay
+
+# Создание пользователя (повторный запуск скрипта не вызовет ошибку)
+#ksmbd.adduser -a "$ARCHIVE_USER" -p "$PASSWORD" 2>/dev/null
+
+# Очистка и создание SMB-шары
+ksmbd.addshare -d $SHARE_NAME 2>/dev/null
+
+ksmbd.addshare -a $SHARE_NAME  -o 'path='$ARCHIVE_DIR -o 'browseable=yes' -o 'writeable=yes' -o 'read only = no' -o 'guest ok = yes' -o 'directory mask = 0777' -o 'create mask = 0666'
+
+chmod 0777 /etc/ksmbd/ksmbd.conf
+/etc/init.d/ksmbd restart
+sleep 3
+
+# Получение IP и hostname
+IP=$(ip -4 addr show br-lan | awk '/inet / {print $2}' | cut -d/ -f1)
+HOST=$(uci get system.@system[0].hostname)
+
+# Вывод итогов
+printf "\n✅ ${YELLOW}Готово! Вставьте одну из ссылок в алресную строку проводника Windows:"; echo; echo
+printf "${YELLOW}\\\\\\\\%s\\\\%s${NC}\n" "$IP" "$SHARE_NAME"
+printf "${YELLOW}\\\\\\\\%s.lan\\\\%s${NC}\n" "$HOST" "$SHARE_NAME"
+echo; echo;
